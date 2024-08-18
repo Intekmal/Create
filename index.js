430130af0f440a11718e3e78cb0b214ce82a476f
Intekmal <jisankhan5125@gmail.com>
2024-08-18 22:23:53 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,142 +1,128 @@
-import { readFileSync, writeFileSync, existsSync, statSync } from "fs";
-import { spawn, execSync } from "child_process";
-import semver from "semver";
-import axios from "axios";
-
-import {} from "dotenv/config";
-import logger from "./core/var/modules/logger.js";
-import { loadPlugins } from "./core/var/modules/installDep.js";
-
-import {
-    isGlitch,
-    isReplit,
-    isGitHub,
-} from "./core/var/modules/environments.get.js";
-
-console.clear();
-
-// Install newer node version on some old Repls
-function upNodeReplit() {
-    return new Promise((resolve) => {
-        execSync(
-            "npm i --save-dev node@16 && npm config set prefix=$(pwd)/node_modules/node && export PATH=$(pwd)/node_modules/node/bin:$PATH"
-        );
-        resolve();
-    });
-}
-
-(async () => {
-    if (process.version.slice(1).split(".")[0] < 16) {
-        if (isReplit) {
-            try {
-                logger.warn("Installing Node.js v16 for Repl.it...");
-                await upNodeReplit();
-                if (process.version.slice(1).split(".")[0] < 16)
-                    throw new Error("Failed to install Node.js v16.");
-            } catch (err) {
-                logger.error(err);
-                process.exit(0);
-            }
-        }
-        logger.error(
-            "Xavia requires Node 16 or higher. Please update your version of Node."
-        );
-        process.exit(0);
+[
+    {
+        "key": "dbln",
+        "value": "%7B%2261564413724696%22%3A%22SQBcMT9e%22%7D",
+        "domain": "facebook.com",
+        "path": "/login/device-based/",
+        "hostOnly": false,
+        "creation": "2024-08-18T16:23:35.897Z",
+        "lastAccessed": "2024-08-18T16:23:35.903Z"
+    },
+    {
+        "key": "datr",
+        "value": "WcC_ZuIdcs0aADGxLx6Tmh9s",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2024-08-18T16:23:35.903Z",
+        "lastAccessed": "2024-08-18T16:23:35.903Z"
+    },
+    {
+        "key": "sb",
+        "value": "WcC_ZsxN6w7e0jNth-GH1toa",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2024-08-18T16:23:35.903Z",
+        "lastAccessed": "2024-08-18T16:23:35.903Z"
+    },
+    {
+        "key": "m_pixel_ratio",
+        "value": "1.8125",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2024-08-18T16:23:35.903Z",
+        "lastAccessed": "2024-08-18T16:23:35.903Z"
+    },
+    {
+        "key": "ps_l",
+        "value": "1",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2024-08-18T16:23:35.903Z",
+        "lastAccessed": "2024-08-18T16:23:35.903Z"
+    },
+    {
+        "key": "ps_n",
+        "value": "1",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2024-08-18T16:23:35.903Z",
+        "lastAccessed": "2024-08-18T16:23:35.903Z"
+    },
+    {
+        "key": "wd",
+        "value": "398x809",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2024-08-18T16:23:35.903Z",
+        "lastAccessed": "2024-08-18T16:23:35.903Z"
+    },
+    {
+        "key": "c_user",
+        "value": "61564413724696",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2024-08-18T16:23:35.903Z",
+        "lastAccessed": "2024-08-18T16:23:35.903Z"
+    },
+    {
+        "key": "fr",
+        "value": "0pwBVfpP7vbwLwnSl.AWXPrd7gf1bIpjvFJmRWcJ4Z2mk.Bmv8BZ..AAA.0.0.Bmwhhz.AWXeTiJVTDE",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2024-08-18T16:23:35.903Z",
+        "lastAccessed": "2024-08-18T16:23:35.903Z"
+    },
+    {
+        "key": "xs",
+        "value": "14%3AGmovl15sDDzKQA%3A2%3A1723996276%3A-1%3A-1",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2024-08-18T16:23:35.903Z",
+        "lastAccessed": "2024-08-18T16:23:35.903Z"
+    },
+    {
+        "key": "locale",
+        "value": "en_US",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2024-08-18T16:23:35.903Z",
+        "lastAccessed": "2024-08-18T16:23:35.903Z"
+    },
+    {
+        "key": "wl_cbv",
+        "value": "v2%3Bclient_version%3A2597%3Btimestamp%3A1723996292",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2024-08-18T16:23:35.903Z",
+        "lastAccessed": "2024-08-18T16:23:35.903Z"
+    },
+    {
+        "key": "fbl_st",
+        "value": "100431945%3BT%3A28733271",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2024-08-18T16:23:35.903Z",
+        "lastAccessed": "2024-08-18T16:23:35.903Z"
+    },
+    {
+        "key": "vpd",
+        "value": "v1%3B809x398x1.8125",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2024-08-18T16:23:35.903Z",
+        "lastAccessed": "2024-08-18T16:23:35.903Z"
     }
-
-    if (isGlitch) {
-        const WATCH_FILE = {
-            restart: {
-                include: ["\\.json"],
-            },
-            throttle: 3000,
-        };
-
-        if (
-            !existsSync(process.cwd() + "/watch.json") ||
-            !statSync(process.cwd() + "/watch.json").isFile()
-        ) {
-            logger.warn("Glitch environment detected. Creating watch.json...");
-            writeFileSync(
-                process.cwd() + "/watch.json",
-                JSON.stringify(WATCH_FILE, null, 2)
-            );
-            execSync("refresh");
-        }
-    }
-
-    if (isGitHub) {
-        logger.warn("Running on GitHub is not recommended.");
-    }
-})();
-
-// End
-
-// CHECK UPDATE
-async function checkUpdate() {
-    logger.custom("Checking for updates...", "UPDATE");
-    try {
-        const res = await axios.get(
-            "https://raw.githubusercontent.com/XaviaTeam/XaviaBot/main/package.json"
-        );
-
-        const { version } = res.data;
-        const currentVersion = JSON.parse(
-            readFileSync("./package.json")
-        ).version;
-        if (semver.lt(currentVersion, version)) {
-            logger.warn(`New version available: ${version}`);
-            logger.warn(`Current version: ${currentVersion}`);
-        } else {
-            logger.custom("No updates available.", "UPDATE");
-        }
-    } catch (err) {
-        logger.error("Failed to check for updates.");
-    }
-}
-
-// Child handler
-const _1_MINUTE = 60000;
-let restartCount = 0;
-
-async function main() {
-    await checkUpdate();
-    await loadPlugins();
-    const child = spawn(
-        "node",
-        [
-            "--trace-warnings",
-            "--experimental-import-meta-resolve",
-            "--expose-gc",
-            "core/_build.js",
-        ],
-        {
-            cwd: process.cwd(),
-            stdio: "inherit",
-            env: process.env,
-        }
-    );
-
-    child.on("close", async (code) => {
-        handleRestartCount();
-        if (code !== 0 && restartCount < 5) {
-            console.log();
-            logger.error(`An error occurred with exit code ${code}`);
-            logger.warn("Restarting...");
-            await new Promise((resolve) => setTimeout(resolve, 2000));
-            main();
-        } else {
-            console.log();
-            logger.error("XaviaBot has stopped, press Ctrl + C to exit.");
-        }
-    });
-}
-
-function handleRestartCount() {
-    restartCount++;
-    setTimeout(() => {
-        restartCount--;
-    }, _1_MINUTE);
-}
-
-main();
+]
